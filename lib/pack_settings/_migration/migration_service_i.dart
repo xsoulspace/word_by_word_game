32bfad2f36314b865f3add22b39ec41d095f2bc6
Anton Malofeev <anton@xsoulspace.dev>
2022-04-19 20:52:51 +0200

    wip: pack_settings structure

---
+++ lib/pack_settings/_migration/migration_service_i.dart
@@ -0,0 +1,3 @@
+part of pack_settings;
+
+abstract class MigrationServiceI {}
--- lib/pack_settings/pack_settings.dart
+++ lib/pack_settings/pack_settings.dart
@@ -7,8 +7,12 @@ import 'package:word_by_word_game/_library/hooks/hooks.dart';
 import 'package:word_by_word_game/_utils/utils.dart';
 import 'package:word_by_word_game/generated/l10n.dart';
 
+part '_migration/a_migration_service.dart';
+part '_migration/migration_service_i.dart';
 part '_services/general_settings_service.dart';
 part '_services/general_settings_service_i.dart';
+part '_services/profile_service.dart';
+part '_services/profile_service_i.dart';
 part '_states/game_loading_statuses.dart';
 part '_states/general_settings_notifier.dart';
 part 'settings/settings_screen.dart';
