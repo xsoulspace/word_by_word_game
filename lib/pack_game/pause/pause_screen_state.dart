e656b18304b288243c770a7f805778bc628cd64c
Anton Malofeev <anton@xsoulspace.dev>
2022-10-23 00:29:31 +0300

    fix: allow links

--- lib/pack_game/pause/pause_screen_state.dart
+++ lib/pack_game/pause/pause_screen_state.dart
@@ -1,6 +1,6 @@
 part of 'pause_screen.dart';
 
-bool get linksAreAllowed => false;
+bool get linksAreAllowed => true;
 
 class _PauseScreenStateDiDto {
   _PauseScreenStateDiDto.use(final Locator read)
