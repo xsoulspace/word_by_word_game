867f2e5ef22ffe1c4b86d15434d7a52759a0f090
Anton Malofeev <anton@xsoulspace.dev>
2022-12-17 14:55:56 +0200

    fix: character avatar

--- lib/pack_game/pause/pause_screen.dart
+++ lib/pack_game/pause/pause_screen.dart
@@ -12,6 +12,7 @@ import 'package:wbw_design_core/wbw_design_core.dart';
 import 'package:word_by_word_game/generated/l10n.dart';
 import 'package:word_by_word_game/pack_core/global_states/global_states.dart';
 import 'package:word_by_word_game/pack_core/pack_core.dart';
+import 'package:word_by_word_game/pack_game/levels/levels.dart';
 import 'package:word_by_word_game/pack_game/mechanics/mechanics.dart';
 import 'package:word_by_word_game/pack_game/pause/widgets/start_game_hex.dart';
 import 'package:yandex_ads_sdk/yandex_ads_sdk.dart';
@@ -23,6 +24,7 @@ class PauseScreen extends HookWidget {
     final Key? key,
   }) : super(key: key);
   static const _kIsPrivacyPolicyEnabled = false;
+  static const _kIsCharacterVisible = false;
   @override
   Widget build(final BuildContext context) {
     final state = _usePauseScreenState(read: context.read);
@@ -32,48 +34,58 @@ class PauseScreen extends HookWidget {
       create: (final context) => state,
       builder: (final context, final child) {
         return Scaffold(
-          body: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const TopSafeArea(),
-                const Spacer(),
-                const StartGameHex(),
-                const Spacer(),
-                Wrap(
-                  alignment: WrapAlignment.center,
-                  spacing: 2,
-                  runSpacing: 12,
+          body: Stack(
+            children: [
+              Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    UiFilledButton.icon(
-                      icon: Icons.scoreboard_rounded,
-                      text: S.of(context).playersAndHighscore,
-                      onPressed: state.onToPlayersAndHighscore,
+                    const TopSafeArea(),
+                    const Spacer(),
+                    const StartGameHex(),
+                    const Spacer(),
+                    Wrap(
+                      alignment: WrapAlignment.center,
+                      spacing: 2,
+                      runSpacing: 12,
+                      children: [
+                        UiFilledButton.icon(
+                          icon: Icons.settings,
+                          text: S.of(context).settings,
+                          onPressed: state.onToSettings,
+                        ),
+                        uiTheme.horizontalBoxes.medium,
+                        UiFilledButton.icon(
+                          icon: Icons.scoreboard_rounded,
+                          text: S.of(context).playersAndHighscore,
+                          onPressed: state.onToPlayersAndHighscore,
+                        ),
+                        uiTheme.horizontalBoxes.medium,
+                        UiFilledButton.icon(
+                          icon: Icons.question_mark_rounded,
+                          text: S.of(context).about,
+                          onPressed: state.onShowAbout,
+                        ),
+                        uiTheme.horizontalBoxes.medium,
+                        if (_kIsPrivacyPolicyEnabled)
+                          UiTextButton.text(
+                            text: S.of(context).privacyPolicy,
+                            onPressed: state.onPrivacyPolicy,
+                          ),
+                      ],
                     ),
-                    uiTheme.horizontalBoxes.medium,
-                    UiFilledButton.icon(
-                      icon: Icons.settings,
-                      text: S.of(context).settings,
-                      onPressed: state.onToSettings,
-                    ),
-                    uiTheme.horizontalBoxes.medium,
-                    UiFilledButton.icon(
-                      icon: Icons.question_mark_rounded,
-                      text: S.of(context).about,
-                      onPressed: state.onShowAbout,
-                    ),
-                    uiTheme.horizontalBoxes.medium,
-                    if (_kIsPrivacyPolicyEnabled)
-                      UiTextButton.text(
-                        text: S.of(context).privacyPolicy,
-                        onPressed: state.onPrivacyPolicy,
-                      ),
+                    uiTheme.verticalBoxes.extraLarge,
+                    const BottomSafeArea(),
                   ],
                 ),
-                uiTheme.verticalBoxes.extraLarge,
-                const BottomSafeArea(),
-              ],
-            ),
+              ),
+              if (_kIsCharacterVisible)
+                const Positioned(
+                  right: 24,
+                  top: 24,
+                  child: CharacterAvatarButton.useDefault(),
+                ),
+            ],
           ),
         );
       },
