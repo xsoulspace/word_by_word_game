9697c60622ad4cf769873c01235341487faade79
Anton Malofeev <anton@xsoulspace.dev>
2022-12-20 16:37:33 +0200

    fix: window toolbar for native

--- lib/pack_core/app/app_scaffold.dart
+++ lib/pack_core/app/app_scaffold.dart
@@ -1,3 +1,5 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
@@ -5,6 +7,7 @@ import 'package:life_hooks/life_hooks.dart';
 import 'package:provider/provider.dart';
 import 'package:wbw_core/wbw_core.dart';
 import 'package:wbw_design_core/wbw_design_core.dart';
+import 'package:window_manager/window_manager.dart';
 import 'package:word_by_word_game/generated/l10n.dart';
 import 'package:word_by_word_game/pack_core/app/app_services_provider.dart';
 import 'package:word_by_word_game/pack_core/global_states/global_state_initializer.dart';
@@ -111,15 +114,130 @@ class AppScaffoldBuilder extends HookWidget {
           builder: (final context, final child) {
             return UiTheme(
               scheme: UiThemeScheme.m3(context),
-              child: StateLoader(
-                initializer: GlobalStateInitializer(),
-                loader: const LoadingScreen(),
-                child: child!,
+              child: WindowControlsScaffold(
+                child: StateLoader(
+                  initializer: GlobalStateInitializer(),
+                  loader: const LoadingScreen(),
+                  child: child!,
+                ),
               ),
             );
           },
         );
       },
+    );
+  }
+}
+
+class WindowControlsScaffold extends HookWidget {
+  const WindowControlsScaffold({
+    required this.child,
+    super.key,
+  });
+  final Widget child;
+  @override
+  Widget build(final BuildContext context) {
+    if (kIsWeb || DeviceRuntimeType.isMobile || DeviceRuntimeType.isApple) {
+      return child;
+    }
+
+    final isHoveredNotifier = useIsBool();
+    const dimension = 13.5;
+    return Stack(
+      children: [
+        child,
+        Positioned(
+          top: 0,
+          left: 0,
+          child: FocusableActionDetector(
+            onShowHoverHighlight: (final value) {
+              isHoveredNotifier.value = value;
+            },
+            child: Padding(
+              padding: const EdgeInsets.only(top: 7, left: 7),
+              child: ValueListenableBuilder(
+                valueListenable: isHoveredNotifier,
+                builder: (final context, final isHovered, final child) {
+                  return Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      IconButton(
+                        padding: EdgeInsets.zero,
+                        onPressed: windowManager.close,
+                        icon: isHovered
+                            ? const Icon(CupertinoIcons.clear_circled_solid)
+                            : const Icon(CupertinoIcons.circle_filled),
+                        constraints: const BoxConstraints(
+                          maxWidth: dimension,
+                          maxHeight: dimension,
+                        ),
+                        color: Colors.red,
+                        iconSize: dimension,
+                      ),
+                      const SizedBox(width: 7),
+                      IconButton(
+                        padding: EdgeInsets.zero,
+                        onPressed: () async {
+                          final bool isMinimized =
+                              await windowManager.isMinimized();
+                          if (isMinimized) {
+                            await windowManager.restore();
+                          } else {
+                            await windowManager.minimize();
+                          }
+                        },
+                        color: Colors.amber,
+                        icon: isHovered
+                            ? const Icon(CupertinoIcons.circle_filled)
+                            : const Icon(CupertinoIcons.circle_filled),
+                        constraints: const BoxConstraints(
+                          maxWidth: dimension,
+                          maxHeight: dimension,
+                        ),
+                        iconSize: dimension,
+                      ),
+                      const SizedBox(width: 7),
+                      IconButton(
+                        padding: EdgeInsets.zero,
+                        color: Colors.green,
+                        constraints: const BoxConstraints(
+                          maxWidth: dimension,
+                          maxHeight: dimension,
+                        ),
+                        onPressed: () async {
+                          // final isFullScreen = await windowManager.isFullScreen();
+                          // await windowManager.setFullScreen(!isFullScreen);
+                          if (await windowManager.isMaximized()) {
+                            await windowManager.unmaximize();
+                          } else {
+                            await windowManager.maximize();
+                          }
+                        },
+                        icon: Stack(
+                          children: [
+                            const Icon(
+                              CupertinoIcons.circle_filled,
+                            ),
+                            if (isHovered)
+                              const Center(
+                                child: Icon(
+                                  CupertinoIcons.arrow_up_left_arrow_down_right,
+                                  color: Colors.white,
+                                  size: 9,
+                                ),
+                              ),
+                          ],
+                        ),
+                        iconSize: dimension,
+                      )
+                    ],
+                  );
+                },
+              ),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
