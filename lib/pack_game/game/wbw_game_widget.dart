2f10abf17018d5329322bb09dce6aebfd9bc1318
Anton Malofeev <anton@xsoulspace.dev>
2022-12-21 03:06:04 +0200

    feat: HighlightFrame

--- lib/pack_game/game/wbw_game_widget.dart
+++ lib/pack_game/game/wbw_game_widget.dart
@@ -2,7 +2,7 @@ import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
-import 'package:provider/provider.dart';
+import 'package:flutter_portal/flutter_portal.dart';
 import 'package:wbw_design_core/wbw_design_core.dart';
 import 'package:word_by_word_game/pack_core/global_states/global_states.dart';
 import 'package:word_by_word_game/pack_core/navigation/game_router.dart';
@@ -20,63 +20,65 @@ class WbwGameWidget extends HookWidget {
     final spacing = uiTheme.spacing;
 
     return Scaffold(
-      body: DialogStack(
-        builder: (final context, final dialogController) {
-          return Column(
-            children: [
-              Expanded(
-                child: Padding(
-                  padding: EdgeInsets.all(spacing.medium),
-                  child: MouseRegion(
-                    onHover: (final _) {
-                      // TODO(arenukvern): do we need this or not?
-                      if (!gameFocusNode.hasFocus) {
-                        gameFocusNode.requestFocus();
-                      }
-                    },
-                    child: GameWidget<WbwGame>.controlled(
-                      focusNode: gameFocusNode,
-                      gameFactory: () => WbwGame.use(
-                        read: context.read,
-                        dialogController: dialogController,
-                        theme: Theme.of(context),
-                      ),
-                      //Work in progress loading screen on game start
-                      loadingBuilder: (final context) => const Center(
-                        child: CircularProgressIndicator(),
-                      ),
-                      overlayBuilderMap:
-                          const GameOverlayBuilderMapRouter().build(),
-                      //Work in progress error handling
-                      errorBuilder: (final context, final ex) {
-                        //Print the error in th dev console
-                        debugPrint(ex.toString());
-                        return const Center(
-                          child: Text(
-                            'Sorry, something went wrong. Reload me',
-                          ),
-                        );
+      body: Portal(
+        child: DialogStack(
+          builder: (final context, final dialogController) {
+            return Column(
+              children: [
+                Expanded(
+                  child: Padding(
+                    padding: EdgeInsets.all(spacing.medium),
+                    child: MouseRegion(
+                      onHover: (final _) {
+                        // TODO(arenukvern): do we need this or not?
+                        if (!gameFocusNode.hasFocus) {
+                          gameFocusNode.requestFocus();
+                        }
                       },
+                      child: GameWidget<WbwGame>.controlled(
+                        focusNode: gameFocusNode,
+                        gameFactory: () => WbwGame.use(
+                          read: context.read,
+                          dialogController: dialogController,
+                          theme: Theme.of(context),
+                        ),
+                        //Work in progress loading screen on game start
+                        loadingBuilder: (final context) => const Center(
+                          child: CircularProgressIndicator(),
+                        ),
+                        overlayBuilderMap:
+                            const GameOverlayBuilderMapRouter().build(),
+                        //Work in progress error handling
+                        errorBuilder: (final context, final ex) {
+                          //Print the error in th dev console
+                          debugPrint(ex.toString());
+                          return const Center(
+                            child: Text(
+                              'Sorry, something went wrong. Reload me',
+                            ),
+                          );
+                        },
+                      ),
                     ),
                   ),
                 ),
-              ),
-              BlocSelector<LevelBloc, LevelBlocState, bool>(
-                selector: (final state) {
-                  return state is LiveLevelBlocState;
-                },
-                builder: (final context, final isLive) {
-                  if (isLive) {
-                    return const ControlsWidget();
-                  } else {
-                    return const SizedBox();
-                  }
-                },
-              ),
-              const BottomSafeArea()
-            ],
-          );
-        },
+                BlocSelector<LevelBloc, LevelBlocState, bool>(
+                  selector: (final state) {
+                    return state is LiveLevelBlocState;
+                  },
+                  builder: (final context, final isLive) {
+                    if (isLive) {
+                      return const ControlsWidget();
+                    } else {
+                      return const SizedBox();
+                    }
+                  },
+                ),
+                const BottomSafeArea()
+              ],
+            );
+          },
+        ),
       ),
     );
   }
---
+++ packages/wbw_design_core/lib/src/widgets/highlight_frame.dart
@@ -0,0 +1,136 @@
+import 'package:animate_do/animate_do.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_portal/flutter_portal.dart';
+
+enum HighlightPosition {
+  top,
+}
+
+class HighlightFrame extends StatelessWidget {
+  const HighlightFrame({
+    required this.highlighted,
+    required this.highlightPosition,
+    required this.child,
+    super.key,
+  });
+  final Alignment highlightPosition;
+  final bool highlighted;
+  final Widget child;
+
+  Widget get icon {
+    if (highlightPosition == Alignment.topLeft) {
+      return Transform.translate(
+        offset: const Offset(4, 4),
+        child: Transform.rotate(
+          angle: 45,
+          child: const Icon(CupertinoIcons.hand_point_right_fill),
+        ),
+      );
+    } else if (highlightPosition == Alignment.topRight) {
+      return Transform.translate(
+        offset: const Offset(-4, 4),
+        child: Transform.rotate(
+          angle: -45,
+          child: const Icon(CupertinoIcons.hand_point_left_fill),
+        ),
+      );
+    } else if (highlightPosition == Alignment.topCenter) {
+      return const RotatedBox(
+        quarterTurns: 3,
+        child: Icon(CupertinoIcons.hand_point_left_fill),
+      );
+    } else if (highlightPosition == Alignment.bottomLeft) {
+      return Transform.translate(
+        offset: const Offset(8, -4),
+        child: Transform.rotate(
+          angle: -45,
+          child: const Icon(CupertinoIcons.hand_point_right_fill),
+        ),
+      );
+    } else if (highlightPosition == Alignment.bottomRight) {
+      return Transform.translate(
+        offset: const Offset(-8, -2),
+        child: Transform.rotate(
+          angle: 45,
+          child: const Icon(CupertinoIcons.hand_point_left_fill),
+        ),
+      );
+    } else if (highlightPosition == Alignment.bottomCenter) {
+      return const RotatedBox(
+        quarterTurns: 1,
+        child: Icon(CupertinoIcons.hand_point_left_fill),
+      );
+    } else if (highlightPosition == Alignment.centerLeft) {
+      return const RotatedBox(
+        quarterTurns: 0,
+        child: Icon(CupertinoIcons.hand_point_right_fill),
+      );
+    } else if (highlightPosition == Alignment.centerRight) {
+      return const RotatedBox(
+        quarterTurns: 0,
+        child: Icon(CupertinoIcons.hand_point_left_fill),
+      );
+    } else {
+      throw UnsupportedError('$highlightPosition');
+    }
+  }
+
+  Aligned get anchor {
+    if (highlightPosition == Alignment.topLeft) {
+      return const Aligned(
+        follower: Alignment.bottomRight,
+        target: Alignment.topLeft,
+      );
+    } else if (highlightPosition == Alignment.topRight) {
+      return const Aligned(
+        follower: Alignment.bottomLeft,
+        target: Alignment.topRight,
+      );
+    } else if (highlightPosition == Alignment.topCenter) {
+      return const Aligned(
+        follower: Alignment.bottomCenter,
+        target: Alignment.topCenter,
+      );
+    } else if (highlightPosition == Alignment.bottomLeft) {
+      return const Aligned(
+        follower: Alignment.topRight,
+        target: Alignment.bottomLeft,
+      );
+    } else if (highlightPosition == Alignment.bottomRight) {
+      return const Aligned(
+        follower: Alignment.topLeft,
+        target: Alignment.bottomRight,
+      );
+    } else if (highlightPosition == Alignment.bottomCenter) {
+      return const Aligned(
+        follower: Alignment.topCenter,
+        target: Alignment.bottomCenter,
+      );
+    } else if (highlightPosition == Alignment.centerLeft) {
+      return const Aligned(
+        follower: Alignment.centerRight,
+        target: Alignment.centerLeft,
+      );
+    } else if (highlightPosition == Alignment.centerRight) {
+      return const Aligned(
+        follower: Alignment.centerLeft,
+        target: Alignment.centerRight,
+      );
+    } else {
+      throw UnsupportedError('$highlightPosition');
+    }
+  }
+
+  @override
+  Widget build(final BuildContext context) {
+    return PortalTarget(
+      anchor: anchor,
+      visible: highlighted,
+      portalFollower: Bounce(
+        infinite: true,
+        child: icon,
+      ),
+      child: child,
+    );
+  }
+}
