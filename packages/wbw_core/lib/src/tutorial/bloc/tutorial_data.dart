37949b68de17e9987bda645ada84479fd5e7ca63
Anton Malofeev <arenukvern@gmail.com>
2022-12-29 03:45:22 +0200

    wip: data

--- packages/wbw_core/lib/src/tutorial/bloc/tutorial_data.dart
+++ packages/wbw_core/lib/src/tutorial/bloc/tutorial_data.dart
@@ -2,6 +2,150 @@ part of 'tutorial_bloc.dart';
 
 const _tutorialData = TutorialCollectionsDataModel(
   events: {
-    TutorialCollectionsName.levelIntroduction: [],
+    TutorialCollectionsName.levelIntroduction: [
+      TutorialEventModel(
+        localizedMap: LocalizedMap(
+          value: {
+            Languages.en: '',
+            Languages.ru: '',
+            Languages.it: '',
+          },
+        ),
+        gameActions: [],
+        uiItem: TutorialUiItem.addToDictionaryButton,
+      ),
+      TutorialEventModel(
+        localizedMap: LocalizedMap(
+          value: {
+            Languages.en: '',
+            Languages.ru: '',
+            Languages.it: '',
+          },
+        ),
+        gameActions: [],
+        uiItem: TutorialUiItem.confirmWordButton,
+      ),
+      TutorialEventModel(
+        localizedMap: LocalizedMap(
+          value: {
+            Languages.en: '',
+            Languages.ru: '',
+            Languages.it: '',
+          },
+        ),
+        gameActions: [],
+        uiItem: TutorialUiItem.removeLetterButton,
+      ),
+      TutorialEventModel(
+        localizedMap: LocalizedMap(
+          value: {
+            Languages.en: '',
+            Languages.ru: '',
+            Languages.it: '',
+          },
+        ),
+        gameActions: [],
+        uiItem: TutorialUiItem.suggestWordButton,
+      ),
+      TutorialEventModel(
+        localizedMap: LocalizedMap(
+          value: {
+            Languages.en: '',
+            Languages.ru: '',
+            Languages.it: '',
+          },
+        ),
+        gameActions: [],
+        uiItem: TutorialUiItem.enterWordLeft,
+      ),
+      TutorialEventModel(
+        localizedMap: LocalizedMap(
+          value: {
+            Languages.en: '',
+            Languages.ru: '',
+            Languages.it: '',
+          },
+        ),
+        gameActions: [],
+        uiItem: TutorialUiItem.enterWordRight,
+      ),
+      TutorialEventModel(
+        localizedMap: LocalizedMap(
+          value: {
+            Languages.en: '',
+            Languages.ru: '',
+            Languages.it: '',
+          },
+        ),
+        gameActions: [],
+        uiItem: TutorialUiItem.enterWordPhaseFrame,
+      ),
+      TutorialEventModel(
+        localizedMap: LocalizedMap(
+          value: {
+            Languages.en: '',
+            Languages.ru: '',
+            Languages.it: '',
+          },
+        ),
+        gameActions: [],
+        uiItem: TutorialUiItem.selectActionFrame,
+      ),
+      TutorialEventModel(
+        localizedMap: LocalizedMap(
+          value: {
+            Languages.en: '',
+            Languages.ru: '',
+            Languages.it: '',
+          },
+        ),
+        gameActions: [],
+        uiItem: TutorialUiItem.selectEffectFrame,
+      ),
+      TutorialEventModel(
+        localizedMap: LocalizedMap(
+          value: {
+            Languages.en: '',
+            Languages.ru: '',
+            Languages.it: '',
+          },
+        ),
+        gameActions: [],
+        uiItem: TutorialUiItem.applyAndEndTurnButton,
+      ),
+      TutorialEventModel(
+        localizedMap: LocalizedMap(
+          value: {
+            Languages.en: '',
+            Languages.ru: '',
+            Languages.it: '',
+          },
+        ),
+        gameActions: [],
+        uiItem: TutorialUiItem.yourNameLabel,
+      ),
+      TutorialEventModel(
+        localizedMap: LocalizedMap(
+          value: {
+            Languages.en: '',
+            Languages.ru: '',
+            Languages.it: '',
+          },
+        ),
+        gameActions: [],
+        uiItem: TutorialUiItem.yourScoreLabel,
+      ),
+      TutorialEventModel(
+        localizedMap: LocalizedMap(
+          value: {
+            Languages.en: '',
+            Languages.ru: '',
+            Languages.it: '',
+          },
+        ),
+        gameActions: [],
+        uiItem: TutorialUiItem.gameFuelIndicator,
+      ),
+    ],
   },
 );
