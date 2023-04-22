b15f9e346bf6bbdbc216e0768ddaf8b353bdce6c
Anton Malofeev <arenukvern@gmail.com>
2023-04-22 17:14:21 +0200

    Update live_world_bloc.dart

--- packages/map_editor/lib/state/blocs/live/live_world_bloc.dart
+++ packages/map_editor/lib/state/blocs/live/live_world_bloc.dart
@@ -2,4 +2,11 @@ part of '../../state.dart';
 
 class LiveWorldBloc extends Cubit<LiveWorldBlocState> {
   LiveWorldBloc() : super(const LiveWorldBlocState());
+	Future<void> _onLoad(){
+		// Load saved data as Render models
+		// Loading order:
+		// - tileset
+		// - objects
+		// - player
+	}
 }
