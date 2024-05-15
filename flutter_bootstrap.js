82bf5fee524f09c49226252ce0426c52984a06c7
Arenukvern <Arenukvern@users.noreply.github.com>
2024-05-15 22:17:44 +0000

    deploy: 8ae90530aff38db8c997266d326485b19a716cb9

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -6,7 +6,7 @@ Read more: https://developer.mozilla.org/en-US/docs/Web/Security/Secure_Contexts
 if (!window._flutter) {
   window._flutter = {};
 }
-_flutter.buildConfig = {"engineRevision":"077b742550efc76fbf02421459ce6deafe780202","builds":[{"compileTarget":"dart2js","renderer":"auto","mainJsPath":"main.dart.js"}]};
+_flutter.buildConfig = {"engineRevision":"f6344b75dcf861d8bf1f1322780b8811f982e31a","builds":[{"compileTarget":"dart2js","renderer":"auto","mainJsPath":"main.dart.js"}]};
 
 
 // https://github.com/flutter/website/pull/10256/files
