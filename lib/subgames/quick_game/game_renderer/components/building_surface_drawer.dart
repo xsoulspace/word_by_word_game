86386685def95e053d73c0ce468008cfb89c0ebe
Arenukvern <anton@xsoulspace.dev>
2024-05-21 02:51:32 +0200

    feat: building surface selection

--- lib/subgames/quick_game/game_renderer/components/building_surface_drawer.dart
+++ lib/subgames/quick_game/game_renderer/components/building_surface_drawer.dart
@@ -1,6 +1,8 @@
 import 'dart:async';
+import 'dart:ui';
 
 import 'package:flame/components.dart';
+import 'package:flame/events.dart';
 import 'package:flame/extensions.dart';
 import 'package:flutter/material.dart' as material;
 import 'package:map_editor/state/models/models.dart';
@@ -16,10 +18,10 @@ import 'package:word_by_word_game/subgames/quick_game/quick_game.dart';
 class BuildingSurfaceDrawer extends Component
     with HasGameRef<CanvasRendererGame>, HasCanvasRendererRef {
   static material.Paint get _paint => Palette.grey.withAlpha(60).paint();
+
   @override
   FutureOr<void> onLoad() {
     game.dto.buildingCubit.addListener(_handleGuiChanges);
-
     return super.onLoad();
   }
 
@@ -55,7 +57,10 @@ class BuildingSurfaceDrawer extends Component
     final drawableObjects = <_PlacingSurfaceComponent>[];
     final originUtils = OriginVectorUtils.use(origin);
 
-    void checkAndVerify({required final CellPointModel canvasCell}) {
+    void checkAndVerify({
+      required final CellPointModel canvasCell,
+      required final int index,
+    }) {
       final gameCellPoint =
           originUtils.getCurrentCellByTap(canvasCell.toVector2());
       final collisionConsequences =
@@ -65,7 +70,11 @@ class BuildingSurfaceDrawer extends Component
 
       if (collisionConsequences.isEmpty) {
         drawableObjects.add(
-          _PlacingSurfaceComponent(position: canvasCell.toVector2()),
+          _PlacingSurfaceComponent(
+            position: canvasCell.toVector2(),
+            parent: this,
+            index: index,
+          ),
         );
       }
     }
@@ -75,30 +84,92 @@ class BuildingSurfaceDrawer extends Component
       final x = (i < 5 ? -i : i - 5).toDouble();
       final xTile = x * kTileDimension;
       final shiftedCell = playerPosition.translate(xTile, 0);
-      checkAndVerify(canvasCell: shiftedCell.toCellPoint());
+      checkAndVerify(
+        canvasCell: shiftedCell.toCellPoint(),
+        index: i,
+      );
     }
 
     return addAll(drawableObjects);
   }
 
-  void _deleteObjects() => removeAll(children);
+  int? _selectedIndex;
+  // ignore: use_setters_to_change_properties
+  void _onSelect(final int index) {
+    _selectedIndex = index;
+    // TODO(arenukvern): change status
+  }
+
+  void _deleteObjects() {
+    removeAll(children);
+    _selectedIndex = null;
+  }
 }
 
-class _PlacingSurfaceComponent extends PositionComponent {
+class _PlacingSurfaceComponent extends PositionComponent
+    with HoverCallbacks, TapCallbacks {
   _PlacingSurfaceComponent({
+    required this.parent,
+    required this.index,
     super.position,
   }) : super() {
+    _setHovered(false);
     size = Vector2(kTileDimensionDouble, kTileDimensionDouble);
   }
-  static final paint = material.Paint()
-    ..color = Palette.blue.color
-    ..strokeWidth = 2
-    ..style = material.PaintingStyle.stroke;
+  final int index;
+  @override
+  final BuildingSurfaceDrawer parent;
 
   @override
+  void onHoverEnter() {
+    _setHovered(true);
+    super.onHoverEnter();
+  }
+
+  @override
+  void onHoverExit() {
+    _setHovered(false);
+    super.onHoverExit();
+  }
+
+  bool _isHovered = false;
+  void _setHovered(final bool hovered) {
+    _isHovered = hovered;
+    if (hovered) {
+      _borderPaint
+        ..color = Palette.blue.color
+        ..strokeWidth = 4;
+    } else {
+      _borderPaint
+        ..color = Palette.blue.color.withOpacity(0.3)
+        ..strokeWidth = 0.5;
+    }
+  }
+
+  final _objectPaint = material.Paint()
+    ..color = Palette.blue.color
+    ..style = material.PaintingStyle.fill;
+  final _borderPaint = material.Paint()..style = material.PaintingStyle.stroke;
+  bool get _isSelected => index == parent._selectedIndex;
+  late final _rect = Offset.zero & size.toSize();
+  @override
   void render(final Canvas canvas) {
-    canvas.drawRect(Rect.fromPoints(Offset.zero, size.toOffset()), paint);
+    canvas.drawRRect(
+      RRect.fromRectAndRadius(
+        _rect,
+        const Radius.circular(4),
+      ),
+      _borderPaint,
+    );
+    // TODO(arenukvern): draw a building from type
+    if (_isSelected) canvas.drawRect(_rect, _objectPaint);
 
     super.render(canvas);
+  }
+
+  @override
+  void onTapUp(final TapUpEvent event) {
+    parent._onSelect(index);
+    super.onTapUp(event);
   }
 }
