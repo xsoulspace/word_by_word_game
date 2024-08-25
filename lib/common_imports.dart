63167358fc27bd07fc58147e7b7089b9da160e0b
Arenukvern <anton@xsoulspace.dev>
2024-08-26 01:17:11 +0300

    wip: coin movements

--- lib/common_imports.dart
+++ lib/common_imports.dart
@@ -1,6 +1,6 @@
 export 'package:collection/collection.dart';
 export 'package:flutter/material.dart';
-export 'package:flutter_animate/flutter_animate.dart';
+export 'package:flutter_animate/flutter_animate.dart' hide MoveEffect;
 export 'package:flutter_bloc/flutter_bloc.dart';
 export 'package:map_editor/common_imports.dart';
 export 'package:wbw_core/wbw_core.dart';
